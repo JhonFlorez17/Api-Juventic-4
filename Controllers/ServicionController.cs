ba674bd867e7024feec351134008171d8d67a0a7
Jhon Alexander Florez Carreño <jhon17_verde@hotmail.com>
2021-11-25 20:57:50 -0500

    Se agrego CRUD servicios

---
+++ Controllers/ServicionController.cs
@@ -0,0 +1,158 @@
+﻿using JuventicApiReto4.Modelos;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace JuventicApiReto4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public ServicionController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //CONSULTA
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select id,nombre,descripcion,imagen
+                        from 
+                        servicios
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        //ELIMINACION
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        delete from servicios 
+                        where id=@ServicioId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ServicioId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Servicio servi)
+        {
+            string query = @"
+                        update servicios set 
+                        nombre =@ServicioNombre,
+                        descripcion =@ServicioDescripcion,
+                        imagen =@ServicioImagen
+                        where id =@ServicioId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ServicioId", servi.id);
+                    myCommand.Parameters.AddWithValue("@ServicioNombre", servi.nombre);
+                    myCommand.Parameters.AddWithValue("@ServicioDescripcion", servi.descripcion);
+                    myCommand.Parameters.AddWithValue("@ServicioImagen", servi.imagen);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpPost]
+        public JsonResult Post(Modelos.Servicio servi)
+        {
+            string query = @"
+                        insert into servicios 
+                        (nombre,descripcion,imagen) 
+                        values
+                         (@ServicioNombre,@ServicioDescripcion,@ServicioImagen) ;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@PlatoId", servi.id);
+                    myCommand.Parameters.AddWithValue("@ServicioNombre", servi.nombre);
+                    myCommand.Parameters.AddWithValue("@ServicioDescripcion", servi.descripcion);
+                    myCommand.Parameters.AddWithValue("@ServicioImagen", servi.imagen);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+    }
+}
